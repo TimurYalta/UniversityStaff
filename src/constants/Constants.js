4a73e5b16fa3e6d702287c7445c8b5c14c3cb887
nikita <kalinskiy98@gmail.com>
2019-04-25 15:45:25 +0300

    Fix

--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,3 +1,3 @@
 export const QUESTION_TYPES ={CHECK:'checklist', RADIO:'radio', TEXT:'text'};
-export const API_BASE_URL = "http://18.220.76.105:8080/api";
+export const API_BASE_URL = "http://localhost:8080/api";
 export const AUTHORIZATION = "Authorization";
